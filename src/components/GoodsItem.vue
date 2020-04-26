6f22198740b35256b988e230921e32a6d308458d
livinc1117 <249918630@qq.com>
2020-04-26 22:20:07 +0800

    成功获取到数据

---
+++ src/components/GoodsItem.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>goodsitem</div>
+</template>
+
+<script>
+	export default {
+		name: 'GoodsItem',
+		props: {
+			id: Number,
+			img: String,
+			title: String
+		}
+	}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<div class="banner-box">
+			<img src="../assets/img/banner-hb.jpg" alt="">
+		</div>
+		<GoodsItem />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+	// @ is an alias to /src
+	import GoodsItem from '@/components/GoodsItem.vue'
+	const axios = require('axios');
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	axios.get('/api/mall/small/h5/index', {
+
+		})
+		.then(function(response) {
+			console.log(response);
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
+		.then(function() {
+			// always executed
+		});
+
+
+	export default {
+		name: 'Home',
+		components: {
+			GoodsItem
+		}
+	}
 </script>
+<style>
+	.banner-box img {
+		width: 100%;
+	}
+</style>
